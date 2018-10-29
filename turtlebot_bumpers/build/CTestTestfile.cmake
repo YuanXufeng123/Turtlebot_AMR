cd12ec257f49db58ea0459bd22f1eef189729059
YuanXufeng123 <xy4jj@virginia.edu>
2018-10-29 14:27:32 -0400

    CHANGE1

---
+++ turtlebot_bumpers/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot5/turtlebot_bumpers/src
+# Build directory: /home/robot5/turtlebot_bumpers/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(turtlebot_1)
---
+++ turtlebot_bumpers/build/turtlebot_1/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robot5/turtlebot_bumpers/src/turtlebot_1
+# Build directory: /home/robot5/turtlebot_bumpers/build/turtlebot_1
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
